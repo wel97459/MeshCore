366dceb63fa614ed155e4e801c346a9faebd5b95
Frieder Schrempf <frieder@fris.de>
2025-11-20 10:59:37 +0100

    variants: XIAO NRF52: Enable status LED
    Fix the active state of the LEDs (active low) and enable the status
    LED.
    
    Signed-off-by: Frieder Schrempf <frieder@fris.de>

--- variants/xiao_nrf52/variant.h
+++ variants/xiao_nrf52/variant.h
@@ -34,8 +34,9 @@ extern "C"
 #define LED_RED                 (11)
 #define LED_GREEN               (13)
 #define LED_BLUE                (12)
+#define PIN_STATUS_LED          (LED_BLUE)
 
-#define LED_STATE_ON            (1)     // State when LED is litted
+#define LED_STATE_ON            (0)     // State when LED is on
 
 // Buttons
 #define PIN_BUTTON1             (0)
