a49e1ad326d553552db4908daf76b4b670f12c1e
Winston Lowe <wel97459@gmail.com>
2025-12-03 18:51:26 -0800

    Change to a base class NRF52Board

--- variants/rak4631/RAK4631Board.h
+++ variants/rak4631/RAK4631Board.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <MeshCore.h>
 #include <Arduino.h>
+#include <helpers/NRF52Board.h>
 
 // LoRa radio module pins for RAK4631
 #define  P_LORA_DIO_1   47
@@ -24,31 +24,13 @@
 #define SX126X_DIO2_AS_RF_SWITCH  true
 #define SX126X_DIO3_TCXO_VOLTAGE   1.8
 
-// built-ins
-#define  PIN_VBAT_READ    5
-#define  ADC_MULTIPLIER   (3 * 1.73 * 1.187 * 1000)
-
-class RAK4631Board : public mesh::MainBoard {
+class RAK4631Board : public NRF52Board {
 protected:
   uint8_t startup_reason;
 
 public:
   void begin();
   uint8_t getStartupReason() const override { return startup_reason; }
-
-  #define BATTERY_SAMPLES 8
-
-  uint16_t getBattMilliVolts() override {
-    analogReadResolution(12);
-
-    uint32_t raw = 0;
-    for (int i = 0; i < BATTERY_SAMPLES; i++) {
-      raw += analogRead(PIN_VBAT_READ);
-    }
-    raw = raw / BATTERY_SAMPLES;
-
-    return (ADC_MULTIPLIER * raw) / 4096;
-  }
 
   const char* getManufacturerName() const override {
     return "RAK 4631";
