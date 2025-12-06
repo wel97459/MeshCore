dea6aeb26c0a360ef8c1a8b3050da3b932075fa6
taco <taco@sly.nu>
2025-12-06 16:21:17 +1100

    calculate shared_secret on demand

--- src/helpers/ContactInfo.h
+++ src/helpers/ContactInfo.h
@@ -12,6 +12,7 @@ struct ContactInfo {
   mutable bool shared_secret_valid; // flag to indicate if shared_secret has been calculated
   uint8_t out_path[MAX_PATH_SIZE];
   uint32_t last_advert_timestamp;   // by THEIR clock
+  mutable uint8_t shared_secret[PUB_KEY_SIZE];
   uint32_t lastmod;  // by OUR clock
   int32_t gps_lat, gps_lon;    // 6 dec places
   uint32_t sync_since;
