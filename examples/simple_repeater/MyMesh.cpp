615ab5319b7b0bb1c681f7aa2400340bcb774c34
Kevin Le <vinhdat82@gmail.com>
2025-12-13 07:32:26 +0700

    Fixed to call getMCUTemperature once.

--- examples/simple_repeater/MyMesh.cpp
+++ examples/simple_repeater/MyMesh.cpp
@@ -183,9 +183,9 @@ int MyMesh::handleRequest(ClientInfo *sender, uint32_t sender_timestamp, uint8_t
       telemetry.addVoltage(TELEM_CHANNEL_SELF+1, (float)board.getBattMilliVolts() / 1000.0f);
     #endif
 
-    float temperature = (float)board.getMCUTemperature();
+    float temperature = board.getMCUTemperature();
     if(!isnan(temperature)) { // Supported boards with built-in temperature sensor. ESP32-C3 may return NAN
-      telemetry.addTemperature(TELEM_CHANNEL_SELF, (float)board.getMCUTemperature()); // Built-in MCU Temperature
+      telemetry.addTemperature(TELEM_CHANNEL_SELF, temperature); // Built-in MCU Temperature
     }
 
     // query other sensors -- target specific
